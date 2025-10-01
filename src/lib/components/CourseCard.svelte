5f124d19924e2bc0e49adc1206588fa3a04ad336
SonKkc <soncb.xyz@gmail.com>
2025-10-01 10:50:09 +0700

    feat: add CourseCard component and integrate it into the courses page

---
+++ src/lib/components/CourseCard.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let href = "/blog/xa-ut0-is-gold-without-borders-q-and-a-with-usdt-0-co-founder-lorenzo-r";
+	export let imageSrc = "https://cms.firebits.top/assets/24ccfc7a-c4c9-4fc0-af5a-148d26c85aa5";
+	export let imageAlt = "XAUt0 is gold without borders: Q&A with USDT0 co-founder Lorenzo R";
+	export let title = "XAUt0 is gold without borders: Q&A with USDT0 co-founder Lorenzo R";
+	export let description = "As digital assets evolve, we're starting to see more real-world value being brought on-chain, quite literally.";
+</script>
+
+<a
+	class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
+	{href}
+>
+	<div class="relative h-64 w-full overflow-hidden">
+		<img
+			src={imageSrc}
+			alt={imageAlt}
+			class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
+			sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
+			loading="lazy"
+			decoding="async"
+			style="object-fit: cover; width: 100%; height: 100%;"
+		/>
+	</div>
+	<div class="p-6">
+		<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
+			{title}
+		</h3>
+		<p class="text-foreground mt-2 text-sm">
+			{description}
+		</p>
+	</div>
+</a>
--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { scale } from 'svelte/transition';
+	import CourseCard from '$lib/components/CourseCard.svelte';
 	
 	// Dropdown state
 	let isDropdownOpen = false;
@@ -32,6 +33,15 @@
 			isDropdownOpen = false;
 		}
 	}
+
+	// Sample course data
+	const courses = Array(9).fill({
+		href: "/blog/xa-ut0-is-gold-without-borders-q-and-a-with-usdt-0-co-founder-lorenzo-r",
+		imageSrc: "https://cms.firebits.top/assets/24ccfc7a-c4c9-4fc0-af5a-148d26c85aa5",
+		imageAlt: "XAUt0 is gold without borders: Q&A with USDT0 co-founder Lorenzo R",
+		title: "XAUt0 is gold without borders: Q&A with USDT0 co-founder Lorenzo R",
+		description: "As digital assets evolve, we're starting to see more real-world value being brought on-chain, quite literally."
+	});
 </script>
 
 <svelte:window on:click={handleClickOutside} />
@@ -50,7 +60,8 @@
 	<div class="mb-5 flex flex-wrap md:flex-nowrap">
 		<div class="mb-5 w-full md:mr-5 lg:w-[33rem]">
 			<input
-				class="placeholder:text-muted-foreground border-gray-850 flex h-10 w-full rounded-md border bg-transparent px-3 py-5 transition-colors duration-200 file:border-0 file:bg-transparent file:text-sm file:font-medium hover:border-gray-600 focus:border-white focus:ring-0 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+				class="text-muted-foreground placeholder:text-muted-foreground border-gray-850 flex h-10 w-full rounded-md border bg-transparent px-3 py-5 transition-colors duration-200 file:border-0 file:bg-transparent file:text-sm file:font-medium hover:border-gray-600 focus:border-white focus:ring-0 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+				style="box-shadow: none !important; outline: none !important;"
 				placeholder="Search"
 				name="search"
 				type="text"
@@ -128,239 +139,15 @@
 	</div>
 	<div class="flex flex-col items-center">
 		<div class="mt-10 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/xa-ut0-is-gold-without-borders-q-and-a-with-usdt-0-co-founder-lorenzo-r"
-			>
-				<div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/24ccfc7a-c4c9-4fc0-af5a-148d26c85aa5"
-						alt="XAUt0 is gold without borders: Q&amp;A with USDT0 co-founder Lorenzo R"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						XAUt0 is gold without borders: Q&amp;A with USDT0 co-founder Lorenzo R
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						As digital assets evolve, we’re starting to see more real-world value being brought
-						on-chain, quite literally.
-					</p>
-				</div>
-			</a>
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/doge-coin-faces-key-resistance-will-bulls-establish-a-higher-low-before-the-next-leg-up"
-			>
-				<div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/8e4ae129-d66e-46fe-8a56-c2edea6ac672"
-						alt="DOGE coin faces key resistance: will bulls establish a Higher Low before the next leg up?"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						DOGE coin faces key resistance: will bulls establish a Higher Low before the next leg
-						up?
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						Dogecoin has maintained a short-term uptrend, but it now faces a critical technical
-						resistance zone that could trigger a corrective move. While this may pause the current
-						rally, a healthy pullback could establish a bullish higher low, reinforcing trend
-						continuation.
-					</p>
-				</div>
-			</a>
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/bitcoin-skeptical-vanguard-holds-20m-shares-in-strategy-amid-index-inflows"
-			>
-				<div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/1cbb1523-fa1f-481e-b8f6-43e4e92e0b6b"
-						alt="Bitcoin-skeptical Vanguard holds 20m+ shares in Strategy amid index inflows"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						Bitcoin-skeptical Vanguard holds 20m+ shares in Strategy amid index inflows
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						The crypto-skeptical Wall Street giant has significant indirect exposure to Bitcoin,
-						thanks to Strategy and other stocks.
-					</p>
-				</div>
-			</a>
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/why-steampunk-rabbits-are-the-future-of-work"
-			>
-				<div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/3eff7dc2-445a-47c5-9503-3f600ecdb5c6"
-						alt="Why Steampunk Rabbits Are The Future of Work"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						Why Steampunk Rabbits Are The Future of Work
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						This post discusses how steampunk rabbits could be the future of work, highlighting
-						their efficiency, multi-talented nature, and ability to bring charm to the workplace.
-					</p>
-				</div>
-			</a>
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/the-perks-of-going-headless-why-you-should-consider-a-headless-cms"
-			>
-				<div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/fd6440c2-dd48-4792-9d08-3124cd99b40f"
-						alt="The Perks of Going Headless: Why You Should Consider a Headless CMS"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						The Perks of Going Headless: Why You Should Consider a Headless CMS
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						Discover why a headless CMS might be the right choice for your business.
-					</p>
-				</div>
-			</a>
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/testing-one-two-three"
-			>
-				<div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/7775c53a-6c2c-453d-8c22-8b5445121d10"
-						alt="How To Become A Very Productive Rabbit"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						How To Become A Very Productive Rabbit
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						In this blog post, we will look at some simple tips and tricks that you can adopt to
-						become a very productive rabbit.
-					</p>
-				</div>
-			</a>
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/rabbit-facts-that-will-melt-your-mind"
-			>
-				<div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/8f748634-d77b-4985-b27e-7e1f3559881a"
-						alt="Rabbit Facts That Will Blow Your Mind me"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						Rabbit Facts That Will Blow Your Mind me
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						These interesting facts about rabbits make them fascinating creatures that are worth
-						learning more about.
-					</p>
-				</div>
-			</a>
-			<a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/essential-tips-for-keeping-your-bunny-looking-and-feeling-their-best"
-				><div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/d4fd6edc-4cc5-48c1-8bc7-e646924bbdca"
-						alt="Essential Tips for Keeping Your Bunny Looking and Feeling Their Best"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						Essential Tips for Keeping Your Bunny Looking and Feeling Their Best
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						Rabbit grooming, encompassing brushing, nail trimming, ear cleaning, dental care, and
-						spot cleaning, is vital for your bunny's health, appearance, and well-being.
-					</p>
-				</div></a
-			><a
-				class="relative max-w-[350px] overflow-hidden rounded-lg border border-slate-800 transition-colors duration-200 ease-in-out hover:dark:border-[#484354]"
-				href="/blog/pirates-guide-to-productivity-web-developers-favorite-tools-on-macs"
-				><div class="relative h-64 w-full overflow-hidden">
-					<img
-						src="https://cms.firebits.top/assets/9a52e835-e131-4290-81bb-5a512599f93e"
-						alt="A Pirate's Guide to Productivity: Web Developers' Favorite Tools on Macs"
-						class="h-auto w-full object-cover transition-transform duration-300 group-hover:scale-110"
-						sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw"
-						loading="lazy"
-						decoding="async"
-						fill="true"
-						style="object-fit: cover; width: 100%; height: 100%;"
-					/>
-				</div>
-				<div class="p-6">
-					<h3 class="font-heading group-hover:text-accent text-xl transition-colors duration-300">
-						A Pirate's Guide to Productivity: Web Developers' Favorite Tools on Macs
-					</h3>
-					<p class="text-foreground mt-2 text-sm">
-						Ahoy, matey! Discover the top productivity tools web developers be usein' on their
-						trusty Macs. Set sail for smoother workflows and greater bounty!
-					</p>
-				</div></a
-			>
+			{#each courses as course, index}
+				<CourseCard
+					href={course.href}
+					imageSrc={course.imageSrc}
+					imageAlt={course.imageAlt}
+					title={course.title}
+					description={course.description}
+				/>
+			{/each}
 		</div>
 	</div>
 </div>
